fba630eb950d297208e0dc5feed87a3028811282
pkarora <pkrocks127@gmail.com>
2020-05-07 12:48:21 +0530

    better code

--- Kangaroos meet.cpp
+++ Kangaroos meet.cpp
@@ -1,6 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
+        int a,b,c,d,x;
+        cin>>a>>b>>c>>d;
+        x = (a-c)/(d-b);
+        if(abs(d-b)>0 && x%1==0)
+        {
+                cout<<"YES";
+        }
+        else if(d-b==0 && a-c==0)
+        {
+                cout<<"YES";
+        }
+        else{ cout<<"NO"; }
+}
+
+/*int main(){
         int a,b,c,d;
         cin>>a>>b>>c>>d;
         bool check = false;
@@ -22,4 +37,4 @@ int main(){
         }
         else {cout<<"NO";}
         return 0;
-}
+   }*/

861de34e76299d61afd126dd4025f5647c6b2e5e
pkarora <pkrocks127@gmail.com>
2020-05-08 12:53:56 +0530

    messed up!

---
+++ between two sets.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+        int n,m;
+        cin>>n>>m;
+        int arn[n],arm[m];
+        int a = INT_MIN;
+        int b = INT_MAX;
+        for(int i=0; i<n; i++)
+        {
+                cin>>arn[i];
+                a = max(a,arn[i]);
+        }
+        for(int i=0; i<m; i++)
+        {
+                cin>>arm[i];
+                b = min(b,arm[i]);
+        }
+        int summ=0, modn, modm;
+        int count=0;
+        while(a<=b)
+        {
+
+                for(int i=0; i<n; i++)
+                {
+                        if(arn[i]>=a)
+                        {
+                                modn = arn[i]%a;
+                        }
+                }
+                for(int i=0; i<m; i++)
+                {
+                        modm = arm[i]%a;
+                        summ+=modm;
+                }
+                if(summ==0)
+                        count++;
+                a++;
+        }
+        cout<<count;
+        return 0;
+}

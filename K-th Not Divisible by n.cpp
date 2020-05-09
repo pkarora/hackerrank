f315fc19c43a1a0670fb698189ff44e7f3287173
pkarora <pkrocks127@gmail.com>
2020-05-09 20:59:36 +0530

    1.c

---
+++ K-th Not Divisible by n.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+        int n,k;
+        cin>>n>>k;
+        int count=0;
+        int div=1;
+        while(div>=1)
+        {
+                int j = div%n;
+                if(j!=0)
+                {
+                        count++;
+                }
+                if(count==k)
+                        break;
+                div++;
+        }
+        cout<<div;
+
+
+}
+int main()
+
+{
+        int t = 1;
+
+        cin>>t;
+
+        while(t--)
+        {
+                solve();
+        }
+
+        return 0;
+}
